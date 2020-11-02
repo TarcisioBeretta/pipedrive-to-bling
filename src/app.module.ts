3352982115ac7fc13531065ba4059bb68458cc29
Tarcísio Beretta <tarcisio.beretta@gmail.com>
2020-11-01 21:01:45 -0300

    Created modules, services, and controllers that will be used

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { BlingModule } from './bling/bling.module';
+import { PipedriveModule } from './pipedrive/pipedrive.module';
+import { DealModule } from './deal/deal.module';
+import { IntegrationModule } from './integration/integration.module';
 
 @Module({
-  imports: [],
+  imports: [BlingModule, PipedriveModule, DealModule, IntegrationModule],
   controllers: [AppController],
   providers: [AppService],
 })
